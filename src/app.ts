fa4ce7c97516018a4543b2394bd09b719cbf4669
smeebear <soheron@gmail.com>
2022-02-19 09:59:19 -0600

    Create Express app, use middleware

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import { RegisterRoutes } from '../build/routes';
+
+export const app = express();
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(bodyParser.json());
+
+RegisterRoutes(app);
