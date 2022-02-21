d0e48724530d7b1f14c6cb87df0730adb22c8ae1
smeebear <soheron@gmail.com>
2022-02-20 19:48:27 -0600

    Add basic error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Response as ExResponse, Request as ExRequest } from 'express';
+import express, { Response as ExResponse, Request as ExRequest, NextFunction } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import bodyParser from 'body-parser';
+import { ValidateError } from 'tsoa';
 
 import { RegisterRoutes } from '../build/routes';
 
@@ -21,3 +22,24 @@ app.use("/docs", swaggerUI.serve, async (_req: ExRequest, res: ExResponse) => {
 app.use(bodyParser.json());
 
 RegisterRoutes(app);
+
+app.use(function errorHandler(
+    err: unknown,
+    req: ExRequest,
+    res: ExResponse,
+    next: NextFunction
+): ExResponse | void {
+    if (err instanceof ValidateError) {
+        console.warn(`Caught Validation Error for ${req.path}:`, err.fields)
+        return res.status(422).json({
+            message: "Validation Failed",
+            details: err?.fields
+        })
+    }
+    if (err instanceof Error) {
+        return res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+    next();
+})
